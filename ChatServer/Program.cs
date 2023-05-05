0cabf7b872d437a7b234dda8ff034f676f92eb15
Wander-man <wanderman.man.man@gmail.com>
2023-05-05 14:33:10 +0300

    Server connection working

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatServer
 {
     class Program
     {
+        static TcpListener _listener;
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
         }
     }
 }
