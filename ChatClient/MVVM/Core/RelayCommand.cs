d4e4fca3c352e4c2946bb438375b5d09145a7142
Wander-man <wanderman.man.man@gmail.com>
2023-05-08 10:37:02 +0300

    Minor warning fixes on the client

--- ChatClient/MVVM/Core/RelayCommand.cs
+++ ChatClient/MVVM/Core/RelayCommand.cs
@@ -10,7 +10,7 @@ namespace ChatClient.MVVM.Core
     class RelayCommand : ICommand
     {
         private Action<object> execute;
-        private Func<object, bool> canExecute;
+        private Func<object, bool>? canExecute;
 
         public event EventHandler? CanExecuteChanged
         {
@@ -18,18 +18,18 @@ namespace ChatClient.MVVM.Core
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
         {
             this.execute = execute;
             this.canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
             return this.canExecute == null || this.canExecute(parameter);
         }
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
             this.execute(parameter);
         }
